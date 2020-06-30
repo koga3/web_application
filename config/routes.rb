27ea0de56a0d0656c50265bae8fead7b36fe911d
koga3 <shota.koga_weeee@icloud.com>
2020-06-30 21:20:35 +0900

    add change

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   get '/' => 'home#top'
   get 'posts/index'
   get 'posts/top'
+  get 'posts/quiz1' => 'posts#quiz1'
+  get 'posts/quiz2' => 'posts#quiz2'
+  get 'posts/quiz3' => 'posts#quiz3'
+  get 'posts/quiz4' => 'posts#quiz4'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
