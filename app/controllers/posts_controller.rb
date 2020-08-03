12d38317f08ddd3c2f3bb11c757ee04e1652a961
koga3 <shota.koga_weeee@icloud.com>
2020-08-03 10:05:41 +0900

    last

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,4 +4,40 @@ class PostsController < ApplicationController
   end
   def top
   end
+  def quiz1
+  end
+  def answer1
+    if params['answer'] == '4009'
+      redirect_to '/posts/correct1'
+    end
+  end
+  def lovewords
+     redirect_to '/posts/hint1'
+  end
+  def answer2
+    if params['answer'] == '4009'
+      redirect_to '/posts/correct2'
+    end
+  end
+  def lovewords2
+     redirect_to '/posts/hint2'
+  end
+
+  def answer3
+    if params['answer'] == '4009'
+      redirect_to '/posts/correct3'
+    end
+  end
+  def lovewords3
+     redirect_to '/posts/hint3'
+  end
+
+  def answer4
+    if params['answer'] == '4009'
+      redirect_to '/posts/correct4'
+    end
+  end
+  def lovewords4
+     redirect_to '/posts/hint4'
+  end
 end
